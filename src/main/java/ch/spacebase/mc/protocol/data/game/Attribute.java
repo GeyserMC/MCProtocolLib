cb39f01614d1c8bab1121bebee30bf16e1699d0e
Steveice10 <Steveice10@gmail.com>
2014-01-01 09:15:08 -0800

    Added DefaultAttribute constructors to Attribute

--- src/main/java/ch/spacebase/mc/protocol/data/game/Attribute.java
+++ src/main/java/ch/spacebase/mc/protocol/data/game/Attribute.java
@@ -9,6 +9,18 @@ public class Attribute {
 	private double value;
 	private List<AttributeModifier> modifiers;
 	
+	public Attribute(DefaultAttribute type) {
+		this(type.getKey(), type.getDefault());
+	}
+	
+	public Attribute(DefaultAttribute type, double value) {
+		this(type.getKey(), value);
+	}
+	
+	public Attribute(DefaultAttribute type, double value, List<AttributeModifier> modifiers) {
+		this(type.getKey(), value, modifiers);
+	}
+	
 	public Attribute(String key, double value) {
 		this(key, value, new ArrayList<AttributeModifier>());
 	}
