d5e9d0f56367f29165164a9dee7b663032831003
lvalenty <lvalenty@gmail.com>
2014-01-02 21:59:19 -0800

    Fixed encoding of ClientAnimation

--- src/main/java/ch/spacebase/mc/protocol/packet/ingame/client/entity/ClientAnimationPacket.java
+++ src/main/java/ch/spacebase/mc/protocol/packet/ingame/client/entity/ClientAnimationPacket.java
@@ -1,6 +1,8 @@
 package ch.spacebase.mc.protocol.packet.ingame.client.entity;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.spacebase.packetlib.io.NetInput;
 import ch.spacebase.packetlib.io.NetOutput;
@@ -43,9 +45,40 @@ public class ClientAnimationPacket implements Packet {
 	public boolean isPriority() {
 		return false;
 	}
-	
-	public static enum Animation {
-		SWING_ARM;
-	}
 
+	public static enum Animation {
+		NO_ANIMATION(0),
+		SWING_ARM(1),
+		DAMAGE_ANIMATION(2),
+		LEAVE_BED(3),
+		EAT_FOOD(5),
+		CRITICAL_EFFECT(6),
+		MAGIC_CRITICAL_EFFECT(7),
+		UNKNOWN_102(102),
+		CROUCH(104),
+		UNCROUCH(105);
+		
+		private static final Map<Integer, Animation> lookup = 
+			new HashMap<Integer, Animation>();
+
+		static {
+			for (final Animation a : Animation.values()) {
+				lookup.put(a.encode(), a);
+			}
+		}
+		
+		private final int animationId;
+
+		private Animation(final int animationId) {
+			this.animationId = animationId;
+		}
+		
+		public static Animation decode(final int animationId) {
+			return lookup.get(animationId);
+		}
+
+		private Integer encode() {
+			return animationId;
+		}
+	}
 }
