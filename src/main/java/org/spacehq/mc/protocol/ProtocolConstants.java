eb57e0835c610eaa3e67f1bbfdf6e010e0132ab7
Steven Smith <Steveice10@gmail.com>
2015-07-20 12:26:36 -0700

    Update version.

--- src/main/java/org/spacehq/mc/protocol/ProtocolConstants.java
+++ src/main/java/org/spacehq/mc/protocol/ProtocolConstants.java
@@ -3,7 +3,7 @@ package org.spacehq.mc.protocol;
 public class ProtocolConstants {
 
     // General Constants
-    public static final String GAME_VERSION = "1.8";
+    public static final String GAME_VERSION = "1.8.7";
     public static final int PROTOCOL_VERSION = 47;
 
     // General Key Constants
