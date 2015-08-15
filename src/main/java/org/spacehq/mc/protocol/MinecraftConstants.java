0c2b7fdce8c06ca88cd2fb4f75a592b66693159a
Steven Smith <Steveice10@gmail.com>
2015-08-14 17:01:28 -0700

    15w33c

--- src/main/java/org/spacehq/mc/protocol/MinecraftConstants.java
+++ src/main/java/org/spacehq/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package org.spacehq.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "15w33b";
-    public static final int PROTOCOL_VERSION = 56;
+    public static final String GAME_VERSION = "15w33c";
+    public static final int PROTOCOL_VERSION = 57;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
