03f1d7d92d3a37b116bbc7b8601afc06cdf75e68
Final Child <finalchild@ourmc.space>
2016-05-25 16:28:10 +0900

    Fix last commit's code style

--- src/main/java/org/spacehq/mc/protocol/packet/ingame/server/world/ServerBlockValuePacket.java
+++ src/main/java/org/spacehq/mc/protocol/packet/ingame/server/world/ServerBlockValuePacket.java
@@ -70,22 +70,22 @@ public class ServerBlockValuePacket implements Packet {
         int type = in.readUnsignedByte();
         int value = in.readUnsignedByte();
         this.blockId = in.readVarInt() & 0xFFF;
-		
-		if(this.blockId == NOTE_BLOCK) {
+        
+        if(this.blockId == NOTE_BLOCK) {
             this.type = MagicValues.key(NoteBlockValueType.class, type);
-			this.value = new NoteBlockValue(value);
+            this.value = new NoteBlockValue(value);
         } else if(this.blockId == STICKY_PISTON || this.blockId == PISTON) {
             this.type = MagicValues.key(PistonValueType.class, type);
-			this.value = MagicValues.key(PistonValue.class, value);
+            this.value = MagicValues.key(PistonValue.class, value);
         } else if(this.blockId == MOB_SPAWNER) {
             this.type = MagicValues.key(MobSpawnerValueType.class, type);
-			this.value = new MobSpawnerValue();
+            this.value = new MobSpawnerValue();
         } else if(this.blockId == CHEST || this.blockId == ENDER_CHEST || this.blockId == TRAPPED_CHEST) {
             this.type = MagicValues.key(ChestValueType.class, type);
-			this.value = new ChestValue(value);
+            this.value = new ChestValue(value);
         } else {
             this.type = MagicValues.key(GenericBlockValueType.class, type);
-			this.value = new GenericBlockValue(value);
+            this.value = new GenericBlockValue(value);
         }
     }
 
