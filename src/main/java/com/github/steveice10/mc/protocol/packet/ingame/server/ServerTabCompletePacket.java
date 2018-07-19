267b9baca3e329d41ecbb86801a809d20c2c285f
Jonas Herzig <me@johni0702.de>
2018-07-19 11:01:19 +0200

    Update ServerTabCompletePacket

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/ServerTabCompletePacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/ServerTabCompletePacket.java
@@ -1,5 +1,7 @@
 package com.github.steveice10.mc.protocol.packet.ingame.server;
 
+import com.github.steveice10.mc.protocol.data.message.Message;
+import com.github.steveice10.mc.protocol.data.message.TextMessage;
 import com.github.steveice10.mc.protocol.packet.MinecraftPacket;
 import com.github.steveice10.packetlib.io.NetInput;
 import com.github.steveice10.packetlib.io.NetOutput;
@@ -7,33 +9,71 @@ import com.github.steveice10.packetlib.io.NetOutput;
 import java.io.IOException;
 
 public class ServerTabCompletePacket extends MinecraftPacket {
+    private int transactionId;
+    private int start;
+    private int length;
     private String matches[];
+    private Message tooltips[];
 
     @SuppressWarnings("unused")
     private ServerTabCompletePacket() {
     }
 
-    public ServerTabCompletePacket(String matches[]) {
+    public ServerTabCompletePacket(int transactionId, int start, int length, String matches[], Message tooltips[]) {
+        if (tooltips.length != matches.length) {
+            throw new IllegalArgumentException("Length of matches and tooltips must be equal.");
+        }
+        this.transactionId = transactionId;
+        this.start = start;
+        this.length = length;
         this.matches = matches;
+        this.tooltips = tooltips;
+    }
+
+    public int getTransactionId() {
+        return this.transactionId;
+    }
+
+    public int getStart() {
+        return this.start;
+    }
+
+    public int getLength() {
+        return this.length;
     }
 
     public String[] getMatches() {
         return this.matches;
     }
 
+    public Message[] getTooltips() {
+        return this.tooltips;
+    }
+
     @Override
     public void read(NetInput in) throws IOException {
         this.matches = new String[in.readVarInt()];
+        this.tooltips = new TextMessage[this.matches.length];
         for(int index = 0; index < this.matches.length; index++) {
             this.matches[index] = in.readString();
+            if (in.readBoolean()) {
+                this.tooltips[index] = Message.fromString(in.readString());
+            }
         }
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
         out.writeVarInt(this.matches.length);
-        for(String match : this.matches) {
-            out.writeString(match);
+        for(int index = 0; index < this.matches.length; index++) {
+            out.writeString(this.matches[index]);
+            Message tooltip = this.tooltips[index];
+            if (tooltip != null) {
+                out.writeBoolean(true);
+                out.writeString(tooltip.toJsonString());
+            } else {
+                out.writeBoolean(false);
+            }
         }
     }
 }
