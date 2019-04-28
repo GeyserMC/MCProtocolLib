932b09b27d4a249d970ac930f63235d1ca7e4a52
Paul Heidenreich <paul.heidenreich@5minds.de>
2019-04-28 12:56:46 +0200

    Update PlayerPlaceBlockPacket

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/player/ClientPlayerPlaceBlockPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/player/ClientPlayerPlaceBlockPacket.java
@@ -12,24 +12,27 @@ import com.github.steveice10.packetlib.io.NetOutput;
 import java.io.IOException;
 
 public class ClientPlayerPlaceBlockPacket extends MinecraftPacket {
+
     private Position position;
     private BlockFace face;
     private Hand hand;
     private float cursorX;
     private float cursorY;
     private float cursorZ;
+    private boolean insideBlock;
 
     @SuppressWarnings("unused")
     private ClientPlayerPlaceBlockPacket() {
     }
 
-    public ClientPlayerPlaceBlockPacket(Position position, BlockFace face, Hand hand, float cursorX, float cursorY, float cursorZ) {
+    public ClientPlayerPlaceBlockPacket(Position position, BlockFace face, Hand hand, float cursorX, float cursorY, float cursorZ, boolean insideBlock) {
         this.position = position;
         this.face = face;
         this.hand = hand;
         this.cursorX = cursorX;
         this.cursorY = cursorY;
         this.cursorZ = cursorZ;
+        this.insideBlock = insideBlock;
     }
 
     public Position getPosition() {
@@ -56,23 +59,29 @@ public class ClientPlayerPlaceBlockPacket extends MinecraftPacket {
         return this.cursorZ;
     }
 
+    public boolean isInsideBlock() {
+        return insideBlock;
+    }
+
     @Override
     public void read(NetInput in) throws IOException {
+        this.hand = MagicValues.key(Hand.class, in.readVarInt());
         this.position = NetUtil.readPosition(in);
         this.face = MagicValues.key(BlockFace.class, in.readVarInt());
-        this.hand = MagicValues.key(Hand.class, in.readVarInt());
         this.cursorX = in.readFloat();
         this.cursorY = in.readFloat();
         this.cursorZ = in.readFloat();
+        this.insideBlock = in.readBoolean();
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
+        out.writeVarInt(MagicValues.value(Integer.class, this.hand));
         NetUtil.writePosition(out, this.position);
         out.writeVarInt(MagicValues.value(Integer.class, this.face));
-        out.writeVarInt(MagicValues.value(Integer.class, this.hand));
         out.writeFloat(this.cursorX);
         out.writeFloat(this.cursorY);
         out.writeFloat(this.cursorZ);
+        out.writeBoolean(this.insideBlock);
     }
 }
