519c2d13044feaa4ab03194e216157b9b013733d
Paul Heidenreich <paul.heidenreich@5minds.de>
2019-05-19 17:03:28 +0200

    Update to 1.14.1

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.14";
-    public static final int PROTOCOL_VERSION = 477;
+    public static final String GAME_VERSION = "1.14.1";
+    public static final int PROTOCOL_VERSION = 480;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
