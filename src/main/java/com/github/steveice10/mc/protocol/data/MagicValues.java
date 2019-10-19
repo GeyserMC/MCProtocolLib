286c4a19256500be2983a87c894e1535ef5bd5b4
Redned <redned235@gmail.com>
2019-10-19 13:43:40 -0500

    Add nbt_tag mapping in MagicValues

--- src/main/java/com/github/steveice10/mc/protocol/data/MagicValues.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/MagicValues.java
@@ -1165,6 +1165,7 @@ public class MagicValues {
         register(CommandParser.DIMENSION, "minecraft:dimension");
         register(CommandParser.TIME, "minecraft:time");
         register(CommandParser.NBT_COMPOUND_TAG, "minecraft:nbt_compound_tag");
+        register(CommandParser.NBT_TAG, "minecraft:nbt_tag");
 
         register(SuggestionType.ASK_SERVER, "minecraft:ask_server");
         register(SuggestionType.ALL_RECIPES, "minecraft:all_recipes");
