a6e150255b6d0bc9076b00b13c310d03e1ea163a
Paul Heidenreich <paul.heidenreich@5minds.de>
2019-12-01 15:14:50 +0100

    Upgrade to 1.15-pre3

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.15-pre2";
-    public static final int PROTOCOL_VERSION = 566;
+    public static final String GAME_VERSION = "1.15-pre3";
+    public static final int PROTOCOL_VERSION = 567;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
