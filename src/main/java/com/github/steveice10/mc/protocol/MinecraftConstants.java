e33abe75a521f238ac97565ac3d4c4b23c620f3c
Steveice10 <Steveice10@gmail.com>
2019-12-17 16:47:58 -0800

    Update to 1.15.1

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.15";
-    public static final int PROTOCOL_VERSION = 573;
+    public static final String GAME_VERSION = "1.15.1";
+    public static final int PROTOCOL_VERSION = 575;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
