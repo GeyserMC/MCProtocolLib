acc5b7fdbd2d433b272b674f956ac1cab1cb1057
RednedEpic <redned235@gmail.com>
2020-01-21 20:44:23 -0600

    Add support for Minecraft 1.15.2

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.15.1";
-    public static final int PROTOCOL_VERSION = 575;
+    public static final String GAME_VERSION = "1.15.2";
+    public static final int PROTOCOL_VERSION = 578;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
