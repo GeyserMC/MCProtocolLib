8eaefa9cfc6e67ff0152cc2d14f8f98d93dc72a5
Steveice10 <Steveice10@gmail.com>
2020-05-30 10:55:04 -0700

    Add javadocs to BuiltinFlags.

--- src/main/java/com/github/steveice10/packetlib/BuiltinFlags.java
+++ src/main/java/com/github/steveice10/packetlib/BuiltinFlags.java
@@ -1,6 +1,13 @@
 package com.github.steveice10.packetlib;
 
+/**
+ * Built-in PacketLib session flags.
+ */
 public class BuiltinFlags {
+    /**
+     * When set to true, prints exceptions that occur when attempting
+     * to resolve DNS SRV records, rather than silently ignoring them.
+     */
     public static final String PRINT_DNS_ERRORS = "print-dns-errors";
 
     private BuiltinFlags() {
