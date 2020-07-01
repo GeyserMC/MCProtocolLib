080055cb734adc67acda7d75ef7dd58d55896ecb
DoctorMacc <toy.fighter1@gmail.com>
2020-07-01 08:35:09 -0400

    Remove the import, too.

--- src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
@@ -3,7 +3,6 @@ package com.github.steveice10.mc.protocol.data.game.entity.player;
 import com.github.steveice10.mc.protocol.data.MagicValues;
 import com.github.steveice10.mc.protocol.data.game.world.notify.ClientNotificationValue;
 import com.github.steveice10.packetlib.io.NetOutput;
-import com.sun.istack.internal.Nullable;
 
 import java.io.IOException;
 
