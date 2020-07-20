57024e7e87678be4412f6d81bccd3d3d09a55d1d
DoctorMacc <toy.fighter1@gmail.com>
2020-07-19 21:13:59 -0400

    Clean up

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/window/ClientCraftingBookStatePacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/window/ClientCraftingBookStatePacket.java
@@ -1,86 +1,35 @@
 package com.github.steveice10.mc.protocol.packet.ingame.client.window;
 
 import com.github.steveice10.mc.protocol.data.MagicValues;
-import com.github.steveice10.mc.protocol.data.game.window.CraftingBookDataType;
+import com.github.steveice10.mc.protocol.data.game.window.CraftingBookStateType;
 import com.github.steveice10.packetlib.io.NetInput;
 import com.github.steveice10.packetlib.io.NetOutput;
 import com.github.steveice10.packetlib.packet.Packet;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.IOException;
 
+@Data
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientCraftingBookStatePacket implements Packet {
-    @Getter
-    private @NonNull CraftingBookDataType type;
-
-    private int bookId;
-    private boolean filterActive;
+    private @NonNull CraftingBookStateType type;
     private boolean bookOpen;
-
-    public ClientCraftingBookStatePacket(int bookId, boolean filterActive,
-                                        boolean bookOpen) {
-        this.type = CraftingBookDataType.CRAFTING_BOOK_STATUS;
-
-        this.bookId = bookId;
-        this.filterActive = filterActive;
-        this.bookOpen = bookOpen;
-    }
-
-    private void ensureType(CraftingBookDataType type, String what) {
-        if (this.type != type) {
-            throw new IllegalStateException(what + " is only set when type is " + type + " but it is " + this.type);
-        }
-    }
-
-    public int bookId() {
-        this.ensureType(CraftingBookDataType.CRAFTING_BOOK_STATUS, "bookId");
-        return this.bookId;
-    }
-
-    public boolean isFilterActive() {
-        this.ensureType(CraftingBookDataType.CRAFTING_BOOK_STATUS, "filterActive");
-        return this.filterActive;
-    }
-
-    public boolean isBookOpen() {
-        this.ensureType(CraftingBookDataType.CRAFTING_BOOK_STATUS, "bookOpen");
-        return this.bookOpen;
-    }
+    private boolean filterActive;
 
     @Override
     public void read(NetInput in) throws IOException {
-        this.type = MagicValues.key(CraftingBookDataType.class, in.readVarInt());
-        switch(this.type) {
-            case CRAFTING_BOOK_STATUS:
-                this.bookId = in.readVarInt();
-                this.filterActive = in.readBoolean();
-                this.bookOpen = in.readBoolean();
-                break;
-            default:
-                throw new IOException("Unknown crafting book data type: " + this.type);
-        }
+        this.type = MagicValues.key(CraftingBookStateType.class, in.readVarInt());
+        this.bookOpen = in.readBoolean();
+        this.filterActive = in.readBoolean();
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
         out.writeVarInt(MagicValues.value(Integer.class, this.type));
-        switch (this.type) {
-            case CRAFTING_BOOK_STATUS:
-                out.writeInt(this.bookId);
-                out.writeBoolean(this.filterActive);
-                out.writeBoolean(this.bookOpen);
-                break;
-            default:
-                throw new IOException("Unknown crafting book data type: " + this.type);
-        }
+        out.writeBoolean(this.bookOpen);
+        out.writeBoolean(this.filterActive);
     }
 
     @Override
--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/window/ClientDisplayedRecipePacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/window/ClientDisplayedRecipePacket.java
@@ -1,67 +1,27 @@
 package com.github.steveice10.mc.protocol.packet.ingame.client.window;
 
-import com.github.steveice10.mc.protocol.data.MagicValues;
-import com.github.steveice10.mc.protocol.data.game.window.CraftingBookDataType;
 import com.github.steveice10.packetlib.io.NetInput;
 import com.github.steveice10.packetlib.io.NetOutput;
 import com.github.steveice10.packetlib.packet.Packet;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.IOException;
 
+@Data
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientDisplayedRecipePacket implements Packet {
-    @Getter
-    private @NonNull CraftingBookDataType type;
-
-    private String recipeId;
-
-    public ClientDisplayedRecipePacket(@NonNull String recipeId) {
-        this.type = CraftingBookDataType.DISPLAYED_RECIPE;
-
-        this.recipeId = recipeId;
-    }
-
-    private void ensureType(CraftingBookDataType type, String what) {
-        if (this.type != type) {
-            throw new IllegalStateException(what + " is only set when type is " + type + " but it is " + this.type);
-        }
-    }
-
-    public String getRecipeId() {
-        this.ensureType(CraftingBookDataType.DISPLAYED_RECIPE, "recipeId");
-        return this.recipeId;
-    }
+    private @NonNull String recipeId;
 
     @Override
     public void read(NetInput in) throws IOException {
-        this.type = MagicValues.key(CraftingBookDataType.class, in.readVarInt());
-        switch(this.type) {
-            case DISPLAYED_RECIPE:
-                this.recipeId = in.readString();
-                break;
-            default:
-                throw new IOException("Unknown crafting book data type: " + this.type);
-        }
+        this.recipeId = in.readString();
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
-        out.writeVarInt(MagicValues.value(Integer.class, this.type));
-        switch (this.type) {
-            case DISPLAYED_RECIPE:
-                out.writeString(this.recipeId);
-                break;
-            default:
-                throw new IOException("Unknown crafting book data type: " + this.type);
-        }
+        out.writeString(this.recipeId);
     }
 
     @Override
