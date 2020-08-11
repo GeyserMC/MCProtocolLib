eefbf2802b10edaba0d4aac84bccdd2c5fa6c132
D3ATHBRINGER13 <mark70326511@gmail.com>
2020-08-11 01:53:27 +0100

    Fix conflicts

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.16.2-rc1";
-    public static final int PROTOCOL_VERSION = 749;
+    public static final String GAME_VERSION = "1.16.2-rc2";
+    public static final int PROTOCOL_VERSION = 750;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
