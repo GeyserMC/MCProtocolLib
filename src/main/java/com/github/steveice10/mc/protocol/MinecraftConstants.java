1934c3e83d1d63d14fa1a99823ad802de178d4ed
D3ATHBRINGER13 <mark70326511@gmail.com>
2020-08-11 15:13:01 +0100

    bump to full release

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -2,8 +2,8 @@ package com.github.steveice10.mc.protocol;
 
 public class MinecraftConstants {
     // General Constants
-    public static final String GAME_VERSION = "1.16.2-rc2";
-    public static final int PROTOCOL_VERSION = 750;
+    public static final String GAME_VERSION = "1.16.2";
+    public static final int PROTOCOL_VERSION = 751;
 
     // General Key Constants
     public static final String PROFILE_KEY = "profile";
