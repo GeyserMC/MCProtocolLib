6c66d7e27ba7e3890f480e594525e6c80bbd549e
Steveice10 <Steveice10@gmail.com>
2020-08-29 11:16:34 -0700

    Replace specifying proxy with specifying custom auth service clients.

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -1,23 +1,80 @@
 package com.github.steveice10.mc.protocol;
 
-public class MinecraftConstants {
+import com.github.steveice10.mc.auth.data.GameProfile;
+import com.github.steveice10.mc.auth.service.SessionService;
+
+/**
+ * Class containing various constants for Minecraft sessions.
+ */
+public final class MinecraftConstants {
     // General Constants
+
+    /**
+     * Current supported game version.
+     */
     public static final String GAME_VERSION = "1.16.2";
+
+    /**
+     * Current supported protocol version.
+     */
     public static final int PROTOCOL_VERSION = 751;
 
     // General Key Constants
+
+    /**
+     * Session flag where the user {@link GameProfile} is stored.
+     */
     public static final String PROFILE_KEY = "profile";
-    public static final String AUTH_PROXY_KEY = "auth-proxy";
+
+    /**
+     * Session flag for providing a custom {@link SessionService} instance.
+     */
+    public static final String SESSION_SERVICE_KEY = "session-service";
 
     // Client Key Constants
+
+    /**
+     * Session flag where the user's access token is stored. Client only.
+     */
     public static final String ACCESS_TOKEN_KEY = "access-token";
+
+    /**
+     * Session flag for providing a custom server info response handler. Client only.
+     */
     public static final String SERVER_INFO_HANDLER_KEY = "server-info-handler";
+
+    /**
+     * Session flag for providing a custom ping time response handler. Client only.
+     */
     public static final String SERVER_PING_TIME_HANDLER_KEY = "server-ping-time-handler";
 
     // Server Key Constants
+
+    /**
+     * Session flag for determining whether to verify users. Server only.
+     */
     public static final String VERIFY_USERS_KEY = "verify-users";
+
+    /**
+     * Session flag for providing a custom server info response builder. Server only.
+     */
     public static final String SERVER_INFO_BUILDER_KEY = "info-builder";
+
+    /**
+     * Session flag for providing a custom server login handler. Server only.
+     */
     public static final String SERVER_LOGIN_HANDLER_KEY = "login-handler";
+
+    /**
+     * Session flag for storing the current ping time. Server only.
+     */
     public static final String PING_KEY = "ping";
+
+    /**
+     * Session flag for determining the packet compression threshold. Server only.
+     */
     public static final String SERVER_COMPRESSION_THRESHOLD = "compression-threshold";
+
+    private MinecraftConstants() {
+    }
 }
--- src/main/java/com/github/steveice10/mc/protocol/ServerLoginHandler.java
+++ src/main/java/com/github/steveice10/mc/protocol/ServerLoginHandler.java
@@ -1,7 +1,15 @@
 package com.github.steveice10.mc.protocol;
 
+import com.github.steveice10.mc.protocol.data.SubProtocol;
 import com.github.steveice10.packetlib.Session;
 
+/**
+ * Interface for handling a session logging in to a server.
+ */
 public interface ServerLoginHandler {
+    /**
+     * Called when a session completes the initial login process and is now in the {@link SubProtocol}.GAME.
+     * @param session Session that logged in.
+     */
     public void loggedIn(Session session);
 }
