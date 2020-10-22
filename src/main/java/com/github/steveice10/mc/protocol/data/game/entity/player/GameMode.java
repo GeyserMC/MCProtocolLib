971f6eebf935e4a0c300aeeb688c180e837f088c
DoctorMacc <toy.fighter1@gmail.com>
2020-10-21 20:46:44 -0400

    Accomodate for encoding bug

--- src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/GameMode.java
@@ -1,10 +1,6 @@
 package com.github.steveice10.mc.protocol.data.game.entity.player;
 
-import com.github.steveice10.mc.protocol.data.MagicValues;
 import com.github.steveice10.mc.protocol.data.game.world.notify.ClientNotificationValue;
-import com.github.steveice10.packetlib.io.NetOutput;
-
-import java.io.IOException;
 
 public enum GameMode implements ClientNotificationValue {
     SURVIVAL,
