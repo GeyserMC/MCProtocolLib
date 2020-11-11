1b167e197af2846908828649a51ec52c0058d5a3
DoctorMacc <toy.fighter1@gmail.com>
2020-11-11 16:15:44 -0500

    Make all recipes nullable
    Cookable recipes can have a nullable result too - I'm going to assume that all recipe types can too.

--- src/main/java/com/github/steveice10/mc/protocol/data/game/recipe/data/CookedRecipeData.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/recipe/data/CookedRecipeData.java
@@ -11,7 +11,7 @@ import lombok.NonNull;
 public class CookedRecipeData implements RecipeData {
     private final @NonNull String group;
     private final @NonNull Ingredient ingredient;
-    private final @NonNull ItemStack result;
+    private final ItemStack result;
     private final float experience;
     private final int cookingTime;
 }
