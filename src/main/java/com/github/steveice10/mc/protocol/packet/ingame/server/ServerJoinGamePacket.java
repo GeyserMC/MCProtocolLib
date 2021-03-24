b9c0330f38a3ae263603f4f91a0c0a61b722d03e
Marius Metzger <marius@crushedpixel.net>
2021-03-24 03:11:27 +0000

    Fix compilation

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/ServerJoinGamePacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/ServerJoinGamePacket.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
 
 import java.io.IOException;
