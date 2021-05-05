cd187d4cbeab812484ae16fc8711f4e96a3f288b
Camotoy <20743703+Camotoy@users.noreply.github.com>
2021-05-05 12:30:25 -0400

    Update to 21w18a

--- src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
+++ src/main/java/com/github/steveice10/mc/protocol/MinecraftConstants.java
@@ -12,12 +12,12 @@ public final class MinecraftConstants {
     /**
      * Current supported game version.
      */
-    public static final String GAME_VERSION = "21w17a";
+    public static final String GAME_VERSION = "21w18a";
 
     /**
      * Current supported protocol version.
      */
-    public static final int PROTOCOL_VERSION = (1 << 30) | 25;
+    public static final int PROTOCOL_VERSION = (1 << 30) | 26;
 
     // General Key Constants
 
