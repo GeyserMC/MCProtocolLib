4ded30ee8611fbda474fc8f0e12cfb0db9a80908
RednedEpic <redned235@gmail.com>
2021-11-20 13:19:25 -0600

    Bump to pre5

--- src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
+++ src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
@@ -124,8 +124,8 @@ import com.github.steveice10.mc.protocol.packet.status.serverbound.ServerboundSt
 
 public class MinecraftCodec {
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 52)
-            .minecraftVersion("1.18-pre4")
+            .protocolVersion((1 << 30) | 53)
+            .minecraftVersion("1.18-pre5")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(0x00, ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
