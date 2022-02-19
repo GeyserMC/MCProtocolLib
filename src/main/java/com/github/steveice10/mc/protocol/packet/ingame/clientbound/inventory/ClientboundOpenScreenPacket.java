ede68d02c4cbd2e4001745c76460d99bdf305885
MCMDEV <john-m.1@gmx.de>
2022-02-19 21:45:49 +0100

    Let's not break API

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
@@ -18,20 +18,37 @@ import java.io.IOException;
 @With
 @AllArgsConstructor
 public class ClientboundOpenScreenPacket implements Packet {
-    private final int containerId;
-    private final @NonNull ContainerType type;
-    private final @NonNull Component name;
+	private final int containerId;
+	private final @NonNull ContainerType type;
+	private final @NonNull Component title;
 
-    public ClientboundOpenScreenPacket(NetInput in) throws IOException {
-        this.containerId = in.readVarInt();
-        this.type = MagicValues.key(ContainerType.class, in.readVarInt());
-        this.name = DefaultComponentSerializer.get().deserialize(in.readString());
+	public ClientboundOpenScreenPacket(NetInput in) throws IOException {
+		this.containerId = in.readVarInt();
+		this.type = MagicValues.key(ContainerType.class, in.readVarInt());
+		this.title = DefaultComponentSerializer.get().deserialize(in.readString());
+	}
+
+    @Deprecated
+    public ClientboundOpenScreenPacket(int containerId, @NonNull ContainerType type, @NonNull String name) {
+        this.containerId = containerId;
+        this.type = type;
+        this.title = DefaultComponentSerializer.get().deserialize(name);
     }
 
     @Override
-    public void write(NetOutput out) throws IOException {
-        out.writeVarInt(this.containerId);
-        out.writeVarInt(MagicValues.value(Integer.class, this.type));
-        out.writeString(DefaultComponentSerializer.get().serialize(this.name));
-    }
+	public void write(NetOutput out) throws IOException {
+		out.writeVarInt(this.containerId);
+		out.writeVarInt(MagicValues.value(Integer.class, this.type));
+		out.writeString(DefaultComponentSerializer.get().serialize(this.title));
+	}
+
+	@Deprecated
+	public String getName() {
+		return DefaultComponentSerializer.get().serialize(title);
+	}
+
+    @Deprecated
+	public ClientboundOpenScreenPacket withName(String name) {
+		return new ClientboundOpenScreenPacket(this.containerId, this.type, DefaultComponentSerializer.get().deserialize(name));
+	}
 }
