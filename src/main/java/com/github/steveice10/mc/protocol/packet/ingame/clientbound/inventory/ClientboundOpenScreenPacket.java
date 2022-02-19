37450a88ab7f4cb3f75844b182fd1447a85e83dd
MCMDEV <john-m.1@gmx.de>
2022-02-19 15:06:58 +0100

    Make title in ClientboundOpenScreenPacket a component

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
@@ -1,5 +1,6 @@
 package com.github.steveice10.mc.protocol.packet.ingame.clientbound.inventory;
 
+import com.github.steveice10.mc.protocol.data.DefaultComponentSerializer;
 import com.github.steveice10.mc.protocol.data.MagicValues;
 import com.github.steveice10.mc.protocol.data.game.inventory.ContainerType;
 import com.github.steveice10.packetlib.io.NetInput;
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.With;
+import net.kyori.adventure.text.Component;
 
 import java.io.IOException;
 
@@ -18,18 +20,18 @@ import java.io.IOException;
 public class ClientboundOpenScreenPacket implements Packet {
     private final int containerId;
     private final @NonNull ContainerType type;
-    private final @NonNull String name;
+    private final @NonNull Component name;
 
     public ClientboundOpenScreenPacket(NetInput in) throws IOException {
         this.containerId = in.readVarInt();
         this.type = MagicValues.key(ContainerType.class, in.readVarInt());
-        this.name = in.readString();
+        this.name = DefaultComponentSerializer.get().deserialize(in.readString());
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
         out.writeVarInt(this.containerId);
         out.writeVarInt(MagicValues.value(Integer.class, this.type));
-        out.writeString(this.name);
+        out.writeString(DefaultComponentSerializer.get().serialize(this.name));
     }
 }
