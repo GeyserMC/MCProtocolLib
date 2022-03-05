835929d57d4ade6996c4ad18ef70cccf7b99f524
John <30979070+MCMDEV@users.noreply.github.com>
2022-03-05 22:56:57 +0100

    Make title in ClientboundOpenScreenPacket a component (#688)

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/inventory/ClientboundOpenScreenPacket.java
@@ -1,5 +1,6 @@
 package com.github.steveice10.mc.protocol.packet.ingame.clientbound.inventory;
 
+import com.github.steveice10.mc.protocol.data.DefaultComponentSerializer;
 import com.github.steveice10.mc.protocol.data.MagicValues;
 import com.github.steveice10.mc.protocol.data.game.inventory.ContainerType;
 import com.github.steveice10.packetlib.io.NetInput;
@@ -9,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.With;
+import net.kyori.adventure.text.Component;
 
 import java.io.IOException;
 
@@ -16,20 +18,37 @@ import java.io.IOException;
 @With
 @AllArgsConstructor
 public class ClientboundOpenScreenPacket implements Packet {
-    private final int containerId;
-    private final @NonNull ContainerType type;
-    private final @NonNull String name;
+	private final int containerId;
+	private final @NonNull ContainerType type;
+	private final @NonNull Component title;
 
-    public ClientboundOpenScreenPacket(NetInput in) throws IOException {
-        this.containerId = in.readVarInt();
-        this.type = MagicValues.key(ContainerType.class, in.readVarInt());
-        this.name = in.readString();
+	public ClientboundOpenScreenPacket(NetInput in) throws IOException {
+		this.containerId = in.readVarInt();
+		this.type = MagicValues.key(ContainerType.class, in.readVarInt());
+		this.title = DefaultComponentSerializer.get().deserialize(in.readString());
+	}
+
+    @Deprecated
+    public ClientboundOpenScreenPacket(int containerId, @NonNull ContainerType type, @NonNull String name) {
+        this.containerId = containerId;
+        this.type = type;
+        this.title = DefaultComponentSerializer.get().deserialize(name);
     }
 
     @Override
-    public void write(NetOutput out) throws IOException {
-        out.writeVarInt(this.containerId);
-        out.writeVarInt(MagicValues.value(Integer.class, this.type));
-        out.writeString(this.name);
-    }
+	public void write(NetOutput out) throws IOException {
+		out.writeVarInt(this.containerId);
+		out.writeVarInt(MagicValues.value(Integer.class, this.type));
+		out.writeString(DefaultComponentSerializer.get().serialize(this.title));
+	}
+
+	@Deprecated
+	public String getName() {
+		return DefaultComponentSerializer.get().serialize(title);
+	}
+
+    @Deprecated
+	public ClientboundOpenScreenPacket withName(String name) {
+		return new ClientboundOpenScreenPacket(this.containerId, this.type, DefaultComponentSerializer.get().deserialize(name));
+	}
 }
