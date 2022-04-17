5b3f737fdbec93f8f6c333c892ef2c3868dc869b
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2022-04-17 11:56:37 +0100

    Address changes

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/serverbound/inventory/ServerboundSetBeaconPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/serverbound/inventory/ServerboundSetBeaconPacket.java
@@ -5,36 +5,47 @@ import com.github.steveice10.packetlib.io.NetOutput;
 import com.github.steveice10.packetlib.packet.Packet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.With;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.OptionalInt;
 
 @Data
 @With
 @AllArgsConstructor
 public class ServerboundSetBeaconPacket implements Packet {
-    private final Integer primaryEffect;
-    private final Integer secondaryEffect;
+    private final @NonNull OptionalInt primaryEffect;
+    private final @NonNull OptionalInt secondaryEffect;
 
     public ServerboundSetBeaconPacket(NetInput in) throws IOException {
         if (in.readBoolean()) {
-            this.primaryEffect = in.readVarInt();
+            this.primaryEffect = OptionalInt.of(in.readVarInt());
         } else {
-            this.primaryEffect = null;
+            this.primaryEffect = OptionalInt.empty();
         }
         if (in.readBoolean()) {
-            this.secondaryEffect = in.readVarInt();
+            this.secondaryEffect = OptionalInt.of(in.readVarInt());
         } else {
-            this.secondaryEffect = null;
+            this.secondaryEffect = OptionalInt.empty();
         }
     }
 
     @Override
     public void write(NetOutput out) throws IOException {
-        out.writeBoolean(this.primaryEffect > 0);
-        out.writeVarInt(this.primaryEffect);
-        out.writeBoolean(this.secondaryEffect > 0);
-        out.writeVarInt(this.secondaryEffect);
+        if (this.primaryEffect.isPresent()) {
+            out.writeBoolean(true);
+            out.writeVarInt(this.primaryEffect.getAsInt());
+        } else {
+            out.writeBoolean(false);
+        }
+
+        if (this.secondaryEffect.isPresent()) {
+            out.writeBoolean(true);
+            out.writeVarInt(this.secondaryEffect.getAsInt());
+        } else {
+            out.writeBoolean(false);
+        }
     }
 }
