e7de688ecbb2fe0a0e4cbe302cf41763ead523b7
Camotoy <20743703+Camotoy@users.noreply.github.com>
2022-06-27 21:54:43 -0400

    Update for Java 1.19.1

--- src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
+++ src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
@@ -192,9 +192,9 @@ public class MinecraftCodec {
     }
 
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion(759)
+            .protocolVersion(760)
             .helper(() -> new MinecraftCodecHelper(LEVEL_EVENTS, SOUND_NAMES))
-            .minecraftVersion("1.19")
+            .minecraftVersion("1.19.1")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(0x00, ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
