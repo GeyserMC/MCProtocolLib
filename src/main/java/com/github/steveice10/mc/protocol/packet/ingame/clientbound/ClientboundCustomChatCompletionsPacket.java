9ccb04d0bd81910cb212d441d21b5b7f4e46c8e8
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2022-07-06 20:37:13 +0100

    1.19.1-pre3

---
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/ClientboundCustomChatCompletionsPacket.java
@@ -0,0 +1,34 @@
+package com.github.steveice10.mc.protocol.packet.ingame.clientbound;
+
+import com.github.steveice10.mc.protocol.codec.MinecraftCodecHelper;
+import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
+import com.github.steveice10.mc.protocol.data.game.ChatCompletionAction;
+import io.netty.buffer.ByteBuf;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.With;
+
+@Data
+@With
+@AllArgsConstructor
+public class ClientboundCustomChatCompletionsPacket implements MinecraftPacket {
+	private final ChatCompletionAction action;
+	private final String[] entries;
+
+	public ClientboundCustomChatCompletionsPacket(ByteBuf in, MinecraftCodecHelper helper) {
+		this.action = ChatCompletionAction.from(helper.readVarInt(in));
+		this.entries = new String[helper.readVarInt(in)];
+		for (int i = 0; i < this.entries.length; i++) {
+			this.entries[i] = helper.readString(in);
+		}
+	}
+
+	@Override
+	public void serialize(ByteBuf out, MinecraftCodecHelper helper) {
+		helper.writeVarInt(out, this.action.ordinal());
+		helper.writeVarInt(out, this.entries.length);
+		for (String entry : this.entries) {
+			helper.writeString(out, entry);
+		}
+	}
+}
