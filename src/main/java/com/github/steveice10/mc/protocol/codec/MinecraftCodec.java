70b0fa1bd42c28c3d7db886a92e613709dc9e1e2
Camotoy <20743703+Camotoy@users.noreply.github.com>
2023-03-14 07:48:11 -0400

    Prepare for release protocol

--- src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
+++ src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
@@ -199,9 +199,9 @@ public class MinecraftCodec {
     }
 
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 120)
+            .protocolVersion(762)
             .helper(() -> new MinecraftCodecHelper(LEVEL_EVENTS, SOUND_NAMES))
-            .minecraftVersion("1.19.4 Pre-release 1")
+            .minecraftVersion("1.19.4")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(0x00, ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
