014c9d3ee949efebf7213037097139be3bea007d
basaigh <53559772+basaigh@users.noreply.github.com>
2023-06-04 15:02:59 +0100

    Update to 1.20-rc1

--- src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
+++ src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
@@ -199,9 +199,9 @@ public class MinecraftCodec {
     }
 
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 140)
+            .protocolVersion((1 << 30) | 141)
             .helper(() -> new MinecraftCodecHelper(LEVEL_EVENTS, SOUND_NAMES))
-            .minecraftVersion("1.20-pre7")
+            .minecraftVersion("1.20-rc1")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(0x00, ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
