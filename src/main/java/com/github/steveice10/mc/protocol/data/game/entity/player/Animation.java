2a8bdbc2b093970ecfef723d45c67f3b60931999
Konicai <71294714+Konicai@users.noreply.github.com>
2023-07-31 16:11:31 -0400

    Annotate animation in ClientboundAnimatePacket as Nullable

--- src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/Animation.java
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/Animation.java
@@ -2,6 +2,7 @@ package com.github.steveice10.mc.protocol.data.game.entity.player;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import org.jetbrains.annotations.Nullable;
 
 public enum Animation {
     SWING_ARM(0),
@@ -20,8 +21,9 @@ public enum Animation {
         return id;
     }
 
-    private static Int2ObjectMap<Animation> VALUES = new Int2ObjectOpenHashMap<>();
+    private static final Int2ObjectMap<Animation> VALUES = new Int2ObjectOpenHashMap<>();
 
+    @Nullable
     public static Animation from(int id) {
         return VALUES.get(id);
     }
