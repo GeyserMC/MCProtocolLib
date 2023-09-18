5fffeaa97efd9d082db6fcfe8b4a568064571ce4
Konicai <71294714+konicai@users.noreply.github.com>
2023-09-18 13:40:39 -0400

    1.20.2-rc2

--- src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
+++ src/main/java/com/github/steveice10/mc/protocol/codec/MinecraftCodec.java
@@ -207,9 +207,9 @@ public class MinecraftCodec {
     }
 
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 152)
+            .protocolVersion((1 << 30) | 153)
             .helper(() -> new MinecraftCodecHelper(LEVEL_EVENTS, SOUND_NAMES))
-            .minecraftVersion("1.20.2-rc1")
+            .minecraftVersion("1.20.2-rc2")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
