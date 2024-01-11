4b3e86a0b1ff7c40198461e417663212c3818938
WinPlay02 <winplay02_gh@woberlaender.de>
2024-01-11 18:38:21 +0100

    Fix ClientboundSetTitleTextPacket serialization (#781)

--- src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/title/ClientboundSetTitleTextPacket.java
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/clientbound/title/ClientboundSetTitleTextPacket.java
@@ -2,13 +2,12 @@ package com.github.steveice10.mc.protocol.packet.ingame.clientbound.title;
 
 import com.github.steveice10.mc.protocol.codec.MinecraftCodecHelper;
 import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
-import com.github.steveice10.mc.protocol.data.DefaultComponentSerializer;
 import io.netty.buffer.ByteBuf;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.With;
 import net.kyori.adventure.text.Component;
-import org.jetbrains.annotations.Nullable;
+import lombok.NonNull;
 
 import java.io.IOException;
 
@@ -16,7 +15,7 @@ import java.io.IOException;
 @With
 @AllArgsConstructor
 public class ClientboundSetTitleTextPacket implements MinecraftPacket {
-    private final @Nullable Component text;
+    private final @NonNull Component text;
 
     public ClientboundSetTitleTextPacket(ByteBuf in, MinecraftCodecHelper helper) throws IOException {
         this.text = helper.readComponent(in);
@@ -24,6 +23,6 @@ public class ClientboundSetTitleTextPacket implements MinecraftPacket {
 
     @Override
     public void serialize(ByteBuf out, MinecraftCodecHelper helper) throws IOException {
-        helper.writeString(out, DefaultComponentSerializer.get().serializeOr(this.text, "null"));
+        helper.writeComponent(out, this.text);
     }
 }
