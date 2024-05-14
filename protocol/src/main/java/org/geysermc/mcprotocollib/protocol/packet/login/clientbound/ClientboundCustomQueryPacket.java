f747f81c07ed8a7ca6395f57a2d5c04ebd2ef59f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-14 18:51:13 +0200

    Fix more packets with invalid resource locations

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/login/clientbound/ClientboundCustomQueryPacket.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/login/clientbound/ClientboundCustomQueryPacket.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.With;
+import net.kyori.adventure.key.Key;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftCodecHelper;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
 
@@ -13,19 +14,19 @@ import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
 @AllArgsConstructor
 public class ClientboundCustomQueryPacket implements MinecraftPacket {
     private final int messageId;
-    private final @NonNull String channel;
+    private final @NonNull Key channel;
     private final byte @NonNull [] data;
 
     public ClientboundCustomQueryPacket(ByteBuf in, MinecraftCodecHelper helper) {
         this.messageId = helper.readVarInt(in);
-        this.channel = helper.readString(in);
+        this.channel = helper.readResourceLocation(in);
         this.data = helper.readByteArray(in, ByteBuf::readableBytes);
     }
 
     @Override
     public void serialize(ByteBuf out, MinecraftCodecHelper helper) {
         helper.writeVarInt(out, this.messageId);
-        helper.writeString(out, this.channel);
+        helper.writeResourceLocation(out, this.channel);
         out.writeBytes(this.data);
     }
 }
