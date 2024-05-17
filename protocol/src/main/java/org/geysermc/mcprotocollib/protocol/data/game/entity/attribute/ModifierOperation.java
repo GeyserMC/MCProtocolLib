da8728f55377657c10b6bf544df022e8edf34eee
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-17 10:22:01 +0200

    Use vanilla names for modifieroperation

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/game/entity/attribute/ModifierOperation.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/game/entity/attribute/ModifierOperation.java
@@ -1,7 +1,7 @@
 package org.geysermc.mcprotocollib.protocol.data.game.entity.attribute;
 
 public enum ModifierOperation {
-    ADD,
+    ADD_VALUE,
     ADD_MULTIPLIED_BASE,
     ADD_MULTIPLIED_TOTAL;
 
