9fa4df56aed5cd8de7de713adc31102169274389
basaigh <53559772+basaigh@users.noreply.github.com>
2024-06-12 17:57:16 +0100

    1.21-rc1

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
@@ -225,9 +225,9 @@ public class MinecraftCodec {
     }
 
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 200)
+            .protocolVersion((1 << 30) | 203)
             .helper(() -> new MinecraftCodecHelper(LEVEL_EVENTS, SOUND_NAMES))
-            .minecraftVersion("1.21-pre2")
+            .minecraftVersion("1.21-rc1")
             .state(ProtocolState.HANDSHAKE, PacketStateCodec.builder()
                     .registerServerboundPacket(ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
