3d5ae58a9c3f5698bf6e13eadbaadeb0e482ddcd
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-09-14 00:48:36 +0200

    Fix compression id

--- protocol/src/main/java/org/geysermc/mcprotocollib/network/NetworkConstants.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/network/NetworkConstants.java
@@ -5,6 +5,6 @@ import org.geysermc.mcprotocollib.network.compression.CompressionConfig;
 import org.geysermc.mcprotocollib.network.crypt.EncryptionConfig;
 
 public class NetworkConstants {
-    public static final AttributeKey<CompressionConfig> COMPRESSION_ATTRIBUTE_KEY = AttributeKey.valueOf("compression_threshold");
+    public static final AttributeKey<CompressionConfig> COMPRESSION_ATTRIBUTE_KEY = AttributeKey.valueOf("compression");
     public static final AttributeKey<EncryptionConfig> ENCRYPTION_ATTRIBUTE_KEY = AttributeKey.valueOf("encryption");
 }
