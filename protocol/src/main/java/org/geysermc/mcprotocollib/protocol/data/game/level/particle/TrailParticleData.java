54d3fc21a6e8c82915a0c4f35e844adb536e51c0
onebeastchris <github@onechris.mozmail.com>
2024-11-28 00:43:54 +0800

    Fix TrailParticleData typo

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/game/level/particle/TrailParticleData.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/data/game/level/particle/TrailParticleData.java
@@ -2,5 +2,5 @@ package org.geysermc.mcprotocollib.protocol.data.game.level.particle;
 
 import org.cloudburstmc.math.vector.Vector3d;
 
-public record TraiParticleData(Vector3d target, int color, int duration) implements ParticleData {
+public record TrailParticleData(Vector3d target, int color, int duration) implements ParticleData {
 }
