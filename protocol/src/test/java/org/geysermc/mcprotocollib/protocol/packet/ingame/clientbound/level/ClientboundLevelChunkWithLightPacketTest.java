d26ed90ecf8785d509d727ddb8375672b4451b3a
basaigh <53559772+basaigh@users.noreply.github.com>
2025-03-23 15:51:24 +0000

    Fix test

--- protocol/src/test/java/org/geysermc/mcprotocollib/protocol/packet/ingame/clientbound/level/ClientboundLevelChunkWithLightPacketTest.java
+++ protocol/src/test/java/org/geysermc/mcprotocollib/protocol/packet/ingame/clientbound/level/ClientboundLevelChunkWithLightPacketTest.java
@@ -1,6 +1,5 @@
 package org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.level;
 
-import org.cloudburstmc.nbt.NbtMap;
 import org.geysermc.mcprotocollib.protocol.data.game.level.LightUpdateData;
 import org.geysermc.mcprotocollib.protocol.data.game.level.block.BlockEntityInfo;
 import org.geysermc.mcprotocollib.protocol.data.game.level.block.BlockEntityType;
@@ -9,17 +8,18 @@ import org.junit.jupiter.api.BeforeEach;
 
 import java.util.BitSet;
 import java.util.Collections;
+import java.util.Map;
 
 public class ClientboundLevelChunkWithLightPacketTest extends PacketTest {
     @BeforeEach
     public void setup() {
         this.setPackets(
                 new ClientboundLevelChunkWithLightPacket(0, 0,
-                        new byte[0], NbtMap.EMPTY, new BlockEntityInfo[0],
+                        new byte[0], Map.of(), new BlockEntityInfo[0],
                         new LightUpdateData(new BitSet(), new BitSet(), new BitSet(), new BitSet(), Collections.emptyList(), Collections.emptyList())
                 ),
                 new ClientboundLevelChunkWithLightPacket(1, 1,
-                        new byte[256], NbtMap.EMPTY, new BlockEntityInfo[]{
+                        new byte[256], Map.of(), new BlockEntityInfo[]{
                         new BlockEntityInfo(1, 0, 1, BlockEntityType.CHEST, null)
                 }, new LightUpdateData(new BitSet(), new BitSet(), new BitSet(), new BitSet(), Collections.emptyList(), Collections.emptyList())
                 )
