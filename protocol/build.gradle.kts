0352fb3b33ebe436d79c8b8c28c66b8e01c6c4f6
basaigh <53559772+basaigh@users.noreply.github.com>
2025-07-02 16:51:37 +0100

    1.21.6 Release

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     jacoco
 }
 
-version = "1.21.6-SNAPSHOT"
+version = "1.21.6-1"
 description = "MCProtocolLib is a simple library for communicating with Minecraft clients and servers."
 
 dependencies {
