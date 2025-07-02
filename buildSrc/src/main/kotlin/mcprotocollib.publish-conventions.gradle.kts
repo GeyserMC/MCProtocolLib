f4004310698f9e3793e6619a6db89ae0a7784a4d
basaigh <53559772+basaigh@users.noreply.github.com>
2025-07-02 15:59:48 +0100

    Temporary fix to allow release publishing See GeyserMC/Cumulus@4434093 and KyoriPowered/indra#139

--- buildSrc/src/main/kotlin/mcprotocollib.publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/mcprotocollib.publish-conventions.gradle.kts
@@ -1,11 +1,13 @@
+import java.net.URI
+
 plugins {
     id("mcprotocollib.base-conventions")
     id("net.kyori.indra.publishing")
 }
 
 indra {
-    publishSnapshotsTo("geysermc", "https://repo.opencollab.dev/maven-snapshots")
-    publishReleasesTo("geysermc", "https://repo.opencollab.dev/maven-releases")
+//    publishSnapshotsTo("geysermc", "https://repo.opencollab.dev/maven-snapshots")
+//    publishReleasesTo("geysermc", "https://repo.opencollab.dev/maven-releases")
 
     configurePublications {
         pom {
@@ -13,6 +15,23 @@ indra {
                 name = "GeyserMC"
                 url = "https://github.com/GeyserMC"
             }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "geysermc"
+            url = URI.create(
+                when {
+                    project.version.toString().endsWith("-SNAPSHOT") ->
+                        "https://repo.opencollab.dev/maven-snapshots"
+                    else ->
+                        "https://repo.opencollab.dev/maven-releases"
+                }
+            )
+            credentials(PasswordCredentials::class.java)
         }
     }
 }
