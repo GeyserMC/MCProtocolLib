a71372753fe0b726e6bea3a085e4024964420a29
Eclipse <eclipse@eclipseisoffline.xyz>
2025-09-22 12:32:14 +0000

    25w33a

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/codec/MinecraftCodec.java
@@ -224,8 +224,8 @@ import org.geysermc.mcprotocollib.protocol.packet.status.serverbound.Serverbound
 
 public class MinecraftCodec {
     public static final PacketCodec CODEC = PacketCodec.builder()
-            .protocolVersion((1 << 30) | 260)
-            .minecraftVersion("25w32a")
+            .protocolVersion((1 << 30) | 262)
+            .minecraftVersion("25w33a")
             .state(ProtocolState.HANDSHAKE, MinecraftPacketRegistry.builder()
                     .registerServerboundPacket(ClientIntentionPacket.class, ClientIntentionPacket::new)
             )
