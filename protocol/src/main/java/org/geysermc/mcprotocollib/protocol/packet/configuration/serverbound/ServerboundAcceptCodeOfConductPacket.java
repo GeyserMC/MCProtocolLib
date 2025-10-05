0d573bbe519984ca98500fdae6ddc347f865a37c
onebeastchris <github@onechris.mozmail.com>
2025-10-05 15:17:32 +0200

    Add final static packet instance for ServerboundAcceptCodeOfConductPacket / ServerboundFinishConfigurationPacket

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/configuration/serverbound/ServerboundAcceptCodeOfConductPacket.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/configuration/serverbound/ServerboundAcceptCodeOfConductPacket.java
@@ -1,11 +1,14 @@
 package org.geysermc.mcprotocollib.protocol.packet.configuration.serverbound;
 
 import io.netty.buffer.ByteBuf;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftPacket;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundClientTickEndPacket;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ServerboundAcceptCodeOfConductPacket implements MinecraftPacket {
-
-    public ServerboundAcceptCodeOfConductPacket(ByteBuf in) {}
+    public static final ServerboundAcceptCodeOfConductPacket INSTANCE = new ServerboundAcceptCodeOfConductPacket();
 
     @Override
     public void serialize(ByteBuf out) {}
