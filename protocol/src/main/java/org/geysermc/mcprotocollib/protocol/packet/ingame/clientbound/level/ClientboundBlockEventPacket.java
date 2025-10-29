939815d8c3495f4b813873dcb397721215de8c65
oryxel1 <gamingnoob681@gmail.com>
2025-10-29 22:58:41 +0700

    Added copper chest to block event block ids.

--- protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/ingame/clientbound/level/ClientboundBlockEventPacket.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/protocol/packet/ingame/clientbound/level/ClientboundBlockEventPacket.java
@@ -45,6 +45,8 @@ public class ClientboundBlockEventPacket implements MinecraftPacket {
     private static final int ENDER_CHEST = 399;
     private static final int TRAPPED_CHEST = 468;
     private static final int END_GATEWAY = 665;
+    private static final int COPPER_CHEST_LOWER = 1079;
+    private static final int COPPER_CHEST_HIGHER = 1086;
     private static final int SHULKER_BOX_LOWER = 675;
     private static final int SHULKER_BOX_HIGHER = 691;
     private static final int BELL = 846;
@@ -76,7 +78,7 @@ public class ClientboundBlockEventPacket implements MinecraftPacket {
                 this.type = MobSpawnerValueType.from(rawType - 1);
                 this.value = new MobSpawnerValue();
             } else if (this.blockId == CHEST || this.blockId == ENDER_CHEST || this.blockId == TRAPPED_CHEST
-                || (this.blockId >= SHULKER_BOX_LOWER && this.blockId <= SHULKER_BOX_HIGHER)) {
+                || (this.blockId >= SHULKER_BOX_LOWER && this.blockId <= SHULKER_BOX_HIGHER) || (this.blockId >= COPPER_CHEST_LOWER && this.blockId <= COPPER_CHEST_HIGHER)) {
                 this.type = ChestValueType.from(rawType - 1);
                 this.value = new ChestValue(rawValue);
             } else if (this.blockId == END_GATEWAY) {
