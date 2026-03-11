225c69ec1c58b472b3f8103bded787dc91333823
MansiPatil21 <patilmansi908@gmail.com>
2026-03-11 11:50:43 -0300

    refactor: introduce explaining variables for URI construction in SessionService
    Extract inline URI.create(String.format(...)) calls into named local variables
    hasJoinedUri and profileUri for improved readability and debuggability.

--- protocol/src/main/java/org/geysermc/mcprotocollib/auth/SessionService.java
+++ protocol/src/main/java/org/geysermc/mcprotocollib/auth/SessionService.java
@@ -32,7 +32,7 @@ public class SessionService {
     /**
      * Calculates the server ID from a base string, public key, and secret key.
      *
-     * @param base Base server ID to use.
+     * @param base      Base server ID to use.
      * @param publicKey Public key to use.
      * @param secretKey Secret key to use.
      * @return The calculated server ID.
@@ -53,9 +53,9 @@ public class SessionService {
     /**
      * Joins a server.
      *
-     * @param profile Profile to join the server with.
+     * @param profile             Profile to join the server with.
      * @param authenticationToken Authentication token to join the server with.
-     * @param serverId ID of the server to join.
+     * @param serverId            ID of the server to join.
      * @throws IOException If an error occurs while making the request.
      */
     public void joinServer(GameProfile profile, String authenticationToken, String serverId) throws IOException {
@@ -64,18 +64,21 @@ public class SessionService {
     }
 
     /**
-     * Gets the profile of the given user if they are currently logged in to the given server.
+     * Gets the profile of the given user if they are currently logged in to the
+     * given server.
      *
-     * @param name Name of the user to get the profile of.
+     * @param name     Name of the user to get the profile of.
      * @param serverId ID of the server to check if they're logged in to.
-     * @return The profile of the given user, or null if they are not logged in to the given server.
+     * @return The profile of the given user, or null if they are not logged in to
+     *         the given server.
      * @throws IOException If an error occurs while making the request.
      */
     public GameProfile getProfileByServer(String name, String serverId) throws IOException {
+        URI hasJoinedUri = URI.create(String.format(HAS_JOINED_ENDPOINT,
+                URLEncoder.encode(name, StandardCharsets.UTF_8),
+                URLEncoder.encode(serverId, StandardCharsets.UTF_8)));
         HasJoinedResponse response = HTTPUtils.makeRequest(this.getProxy(),
-                URI.create(String.format(HAS_JOINED_ENDPOINT,
-                        URLEncoder.encode(name, StandardCharsets.UTF_8),
-                        URLEncoder.encode(serverId, StandardCharsets.UTF_8))),
+                hasJoinedUri,
                 null, HasJoinedResponse.class);
         if (response != null && response.id != null) {
             GameProfile result = new GameProfile(response.id, name);
@@ -97,9 +100,12 @@ public class SessionService {
             return;
         }
 
-        MinecraftProfileResponse response = HTTPUtils.makeRequest(this.getProxy(), URI.create(String.format(PROFILE_ENDPOINT, UUIDUtils.convertToNoDashes(profile.getId()))), null, MinecraftProfileResponse.class);
+        URI profileUri = URI.create(String.format(PROFILE_ENDPOINT, UUIDUtils.convertToNoDashes(profile.getId())));
+        MinecraftProfileResponse response = HTTPUtils.makeRequest(this.getProxy(), profileUri, null,
+                MinecraftProfileResponse.class);
         if (response == null) {
-            throw new IllegalStateException("Couldn't fetch profile properties for " + profile + " as the profile does not exist.");
+            throw new IllegalStateException(
+                    "Couldn't fetch profile properties for " + profile + " as the profile does not exist.");
         }
 
         profile.setProperties(response.properties);
